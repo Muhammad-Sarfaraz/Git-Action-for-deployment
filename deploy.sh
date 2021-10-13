84591d4138da5a38ce27260725154c4cbf9bf31a
id.sent.me@gmail.com <id.sent.me@gmail.com>
2021-10-13 23:57:05 +0600

    COMMIT

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,14 @@ read COMMIT
 
 git add --all
 git commit -m COMMIT
+
+echo "Enter Branch Name."
 read BRANCH
+
 git checkout -b BRANCH
+
+echo "Branch created successfully."
+
 git push origin BRANCH
 
-echo "Deployed successfully"
+echo "Pushed successfully."
